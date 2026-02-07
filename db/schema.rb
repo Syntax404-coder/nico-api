e0aa7e4258193696f0a08f2c8664c47a49df6141
Nico <domenictaganahan@gmail.com>
2026-02-08 00:12:56 +0800

    login bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_04_012505) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_06_143000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -92,7 +92,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_04_012505) do
     t.text "bio"
     t.date "birthdate"
     t.string "city"
-    t.string "country"
+    t.string "country", default: "Philippines"
     t.datetime "created_at", null: false
     t.string "email"
     t.string "first_name"
@@ -101,10 +101,10 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_04_012505) do
     t.string "last_name"
     t.string "mobile"
     t.string "password_digest"
+    t.string "province"
     t.string "role"
     t.string "school"
     t.string "sexual_orientation"
-    t.string "state"
     t.datetime "updated_at", null: false
   end
 
